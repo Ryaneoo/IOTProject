d91f57374eb1dcf99aedef973598dbcf1a8517dd
r <ryannn302007@gmail.com>
2026-02-02 13:48:34 +0800

    Added comments

--- Components/Models/Readings.cs
+++ Components/Models/Readings.cs
@@ -1,4 +1,4 @@
-﻿public class SensorReading
+﻿public class SensorReading // initalise a class to contain the necessary variables to retrieve and display the corresponding data
 {
     public double Temperature { get; set; }
     public double Humidity { get; set; }
